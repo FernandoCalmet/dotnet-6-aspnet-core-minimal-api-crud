d561d21f45b6d4ae74714a425e2dcde054d0ff43
Fernando Calmet <fercalmet@gmail.com>
2022-03-06 12:36:32 -0500

    minor changes

--- MinimalCRUDWebAPI/Program.cs
+++ MinimalCRUDWebAPI/Program.cs
@@ -22,53 +22,64 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-// Routes
+#region : Methods
+
 async Task<List<UserModel>> GetUsers(DataContext context) =>
     await context.Users.ToListAsync();
 
+#endregion : Methods
+
+
+#region : Routes
+// Home
 app.MapGet("/", () => "Hello World! 🔥");
 
+// GetAllUsers
 app.MapGet("/users", async (DataContext context) =>
-    await context.Users.ToListAsync()
+    Results.Ok(await GetUsers(context))
 );
 
+// GetUserById
 app.MapGet("/users/{id}", async (DataContext context, int id) =>
     await context.Users.FindAsync(id) is UserModel user ?
         Results.Ok(user) :
-        Results.NotFound("Sorry, user not found. :/")
+        Results.NotFound("Sorry, user not found.")
 );
 
+// CreateUser
 app.MapPost("/users", async (DataContext context, UserModel user) =>
 {
     context.Users.Add(user);
     await context.SaveChangesAsync();
+
     return Results.Ok(await GetUsers(context));
 });
 
+// UpdateUser
 app.MapPut("/users/{id}", async (DataContext context, UserModel user, int id) =>
 {
     var dbUser = await context.Users.FindAsync(id);
     if (dbUser == null)
-        return Results.NotFound("User not found. :/");
-
+        return Results.NotFound("Sorry, user not found.");
     dbUser.FirstName = user.FirstName;
     dbUser.LastName = user.LastName;
-
     await context.SaveChangesAsync();
 
     return Results.Ok(await GetUsers(context));
 });
 
+// DeleteUser
 app.MapDelete("/users/{id}", async (DataContext context, int id) =>
 {
     var dbUser = await context.Users.FindAsync(id);
     if (dbUser == null)
-        return Results.NotFound("User not found. :/");
-
+        return Results.NotFound("Sorry, user not found.");
     context.Users.Remove(dbUser);
     await context.SaveChangesAsync();
 
     return Results.Ok(await GetUsers(context));
 });
+
+#endregion : Routes
 
 app.Run();
